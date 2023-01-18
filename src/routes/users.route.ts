1bd964365c6a4a76c11c797d0da2e0bb0ca5284a
Jorge Martins <77112497+jorginskn@users.noreply.github.com>
2023-01-18 00:42:04 -0300

    feat:   pegar o usuario por id

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -11,8 +11,14 @@ const usersRoute = Router();
 usersRoute.get("/users", (req: Request, res: Response, next: NextFunction) => {
   const users: object[] = [{ userName: "Jorge" }];
   res.status(200).send(users);
-
-  res.json(users);
 });
+
+usersRoute.get(
+  "/users/:uuid",
+  (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+    const uuid = req.params.uuid;
+    res.status(200).send({ uuid });
+  }
+);
 
 export default usersRoute;
